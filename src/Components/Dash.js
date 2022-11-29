77e4e4399280d439512aea4f83a07e77e161a74f
Oussama Boussaid <87953304+oussama95boussaid@users.noreply.github.com>
2022-11-29 16:31:18 +0100

    start

---
+++ src/Components/Dash.js
@@ -0,0 +1,13 @@
+import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from "react";
+
+function Dash() {
+
+    return (
+        <div>
+            <p>oussama</p>
+        </div>
+    );
+  }
+  
+  export default Dash;
---
+++ src/Pages/MainDash.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dash from "../Components/Dash";
+
+function MainDash() {
+  return (
+    <div>
+      <Dash />
+    </div>
+  );
+}
+
+export default MainDash;
