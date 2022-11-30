9269c1801d01a359a901b3df8128294ad3afc2da
MoncefKa <moncef.karmaoui@e-polytechnique.ma>
2022-11-30 15:56:40 +0100

    noting

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -14,38 +14,39 @@ const Sidebar = () => {
     const Menus = [
         { title: 'Home', path: '/', src: <BiHome /> },
         { title: 'Patients', path: '/Patient', src: <BiGroup /> },
-        { title: 'Log Out', path: '/login', src: <TbLogout />, gap: 'true' },
+        { title: 'LOGOUT', path: '/login', src: <TbLogout />,gap:"true"},
     ]
-
+    const openSide=()=>{
+        setOpen(true);
+    }
+    const closeSide=()=>{
+        setOpen(false)
+    }
     return (
         <>
             <div
                 className={`${
-                    open ? 'w-30' : 'w-fit'
+                    open ? 'w-30 h-full' : 'w-fit h-full'
                 } hidden sm:block relative h-screen duration-300 bg-gray-100 border-r border-gray-200 dark:border-gray-600 p-3 dark:bg-slate-800`}
+                onMouseOver={openSide}
+                onMouseLeave={closeSide}
             >
-                <BsArrowLeftCircle
-                    className={`${
-                        !open && 'rotate-180'
-                    } absolute text-3xl bg-white fill-slate-800  rounded-full cursor-pointer bottom-8   -right-4 dark:fill-gray-400 dark:bg-gray-800`}
-                    onClick={() => setOpen(!open)}
-                />
                 <Link to='/'>
                     <div className={`flex ${open && 'gap-x-1'} items-center`}>
                         <img src={Logo} alt='' className=' w-16 h-16' />
                         {open && (
-                            <span className='text-xl pr-4 font-medium whitespace-nowrap dark:text-white'>
-                                Fab Lab
+                            <span className='text-xl pr-4 font-medium whitespace-nowrap dark:text-white uppercase'>
+                                FabLab
                             </span>
                         )}
                     </div>
                 </Link>
 
-                <ul className='pt-6 '>
+                <ul className='pt-6 h-full'>
                     {Menus.map((menu, index) => (
                         <Link to={menu.path} key={index}>
                             <li
-                                className={`flex items-center text-center gap-x-6 p-3 text-base font-normal rounded-lg cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
+                                className={`flex items-center text-center gap-x-6 p-3 text-base font-normal rounded-lg items-stretch cursor-pointer dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700
                         ${menu.gap ? 'mt-9' : 'mt-2'} ${
                                     location.pathname === menu.path &&
                                     'bg-gray-200 dark:bg-gray-700'
@@ -61,6 +62,7 @@ const Sidebar = () => {
                                 </span>
                             </li>
                         </Link>
+                        
                     ))}
                 </ul>
             </div>
