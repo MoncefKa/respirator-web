7f33d43c72302441e3b8c510ce400b31799d35ac
MoncefKa <moncef.karmaoui@e-polytechnique.ma>
2022-12-01 21:24:57 +0100

    hatim-zaml

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-
+import Numpatients from '../Components/Numpatients';
+import Numappa from '../Components/Numappa';
+import Activeapp from '../Components/Activeapp'
 const Dashboard = () => {
     return(
-        
-        <div className="w-full">
-            <div className="h-12 dark:bg-slate-800 rounded">
-                    <h1 className="text-center text-[#EFF5F5] text-2xl p-2">
-                        General Information
-                    </h1>
+        <div className="w-full overflow-hidden">
+        <div className="h-54 dark:bg-slate-800 rounded">
+            <div className="flex justify-between items-center">
+                <h1 className="text-[#EFF5F5] text-2xl p-8">
+                    General Information
+                </h1>
+                
             </div>
         </div>
+        <div class="grid grid-cols-2 grid-rows-2 gap-2 p-12">
+            <div className="... row-span-2"><Activeapp/></div>
+            <div className="... col-start-2 col-span-2"><Numpatients/></div>
+            <div className="... col-start-2 col-span-2"><Numappa/></div>
+        </div>
+
+    </div>
     );
 }
 
