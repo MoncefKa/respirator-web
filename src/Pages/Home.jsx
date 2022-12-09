9e7437dd89d570cf996da90951c6fd29c133af1e
MoncefKa <moncef.karmaoui@e-polytechnique.ma>
2022-12-09 15:56:56 +0100

    somechanges

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import Lungs from "../assets/images/Lungs.jpg";
 const Home = () => {
     return(
-        <div>
-            <h1>fff</h1>
+        <div className="h-screen w-full" style={{backgroundImage:`url(${Lungs})`,               backgroundSize:"cover",backgroundRepeat:"no-repeat",backgroundPosition:"center"}}>
+            <div className="">
+                <h1 className="text-4xl font-bold text-center">Dimitrov is on a secret mission</h1>
+            </div>
         </div>
     );
 }
