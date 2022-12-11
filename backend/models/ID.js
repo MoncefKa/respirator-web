3dd632e82b3ab0b1c58841feaff9a1b65747fb0c
ocotorse <allouchhatim@gmail.com>
2022-12-12 00:35:17 +0100

    a folder with all my objects

--- backend/models/ID.js
+++ backend/models/ID.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 
 const IDSchema = new mongoose.Schema({
-    _id: {
+    idd: {
         type: String,
         required: true,
+        unique: true,
     },
 });
+
+IDSchema.statics.isIDinUse = async function (idd) {
+    if (!idd) throw new Error("invalid email");
+    try {
+      const user = await this.findOne({ idd });
+      if (user) return false;
+  
+      return true;
+    } catch (error) {
+      console.log("error in isIDinUse Method", error.message);
+      return false;
+    }
+  };
 
 module.exports = mongoose.model("ID", IDSchema);
