c038e159807f406fe5135c9e8cb0243d77350797
MoncefKa <moncef.karmaoui@e-polytechnique.ma>
2022-12-14 06:00:30 +0100

    haaeeer

---
+++ frontend/src/Components/infoResp.js
@@ -0,0 +1,108 @@
+const data=[
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 71.01792908,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 71.01792908,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 71.01792908,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 65.75723267,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 71.01792908,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 65.75723267,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 60.49694443,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 5.75723267,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 20.63996124,
+      "FlowRate": 0,
+      "Presure": 60.49694443,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 21.42159271,
+      "FlowRate": 0,
+      "Presure": 60.49694443,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 21.42159271,
+      "FlowRate": 0,
+      "Presure": 65.75723267,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 21.42159271,
+      "FlowRate": 0,
+      "Presure": 71.01792908,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 21.42159271,
+      "FlowRate": 0,
+      "Presure": 71.01792908,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 21.42159271,
+      "FlowRate": 0,
+      "Presure": 65.75723267,
+      "Status": 1,
+      "data": "P1"
+    },
+    {
+      "Temperture": 21.42159271,
+      "FlowRate": 0,
+      "Presure": 65.75723267,
+      "Status": 1,
+      "data": "P1"
+    }
+  ]
+export default data
